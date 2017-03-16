10f0eb9cd5b24ea778877e16beb30208183e2b64
Ruslan <ruslan.bur@gmail.com>
2017-03-16 15:49:22 +0200

    fix some bag in validation

--- src/main/java/com/dev_rus/phone_book/configuration/ValidationConfiguration.java
+++ src/main/java/com/dev_rus/phone_book/configuration/ValidationConfiguration.java
@@ -2,8 +2,6 @@ package com.dev_rus.phone_book.configuration;
 
 import com.dev_rus.phone_book.service.validator.ValidationService;
 import com.dev_rus.phone_book.service.validator.ValidationServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
@@ -14,28 +12,13 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @Configuration
 public class ValidationConfiguration {
 
-/*    @Bean
+    @Bean
     public LocalValidatorFactoryBean localValidatorFactoryBean() {
         return new LocalValidatorFactoryBean();
     }
 
     @Bean
-    public ValidationService validationService2(){
-        return new ValidationServiceImpl(localValidatorFactoryBean().getValidator());
-    }*/
-
-/*---------------------------------------------------------------------------------*/
-
-    private ApplicationContext applicationContext;
-
-    @Autowired
-    public ValidationConfiguration(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-    }
-
-    @Bean
     public ValidationService validationService() {
-        LocalValidatorFactoryBean bean = applicationContext.getBean(LocalValidatorFactoryBean.class);
-        return new ValidationServiceImpl(bean.getValidator());
+        return new ValidationServiceImpl(localValidatorFactoryBean().getValidator());
     }
 }
